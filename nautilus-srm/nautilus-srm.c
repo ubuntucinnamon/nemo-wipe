f3a17fd84e401eeadbcfe5d42368f6fb34d48617
Colomban Wendling <ban@herbesfolles.org>
2010-02-14 02:59:56 +0100

    Update copyright notice

--- nautilus-srm/nautilus-srm.c
+++ nautilus-srm/nautilus-srm.c
@@ -1,7 +1,7 @@
 /*
  *  nautilus-srm - a nautilus extension to wipe file(s) with srm
  * 
- *  Copyright (C) 2009 Colomban Wendling <ban@herbesfolles.org>
+ *  Copyright (C) 2009-2010 Colomban Wendling <ban@herbesfolles.org>
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public
