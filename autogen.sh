dec28f21e32119b7e27f7fb2a7f62ceac5e11c54
Colomban Wendling <ban@herbesfolles.org>
2011-04-26 20:16:03 +0200

    Update i18n-related build system

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,9 @@
 #mkdir -p build/aux          || exit 1
 #mkdir -p build/m4           || exit 1
 #gtkdocize --flavour no-tmpl || exit 1
-gnome-doc-prepare -f        || exit 1
-autoreconf -vfi             || exit 1
+echo "no" | glib-gettextize -f -c     || exit 1
+intltoolize -c -f --automake          || exit 1
+gnome-doc-prepare -f                  || exit 1
+autoreconf -vfi                       || exit 1
 
 #~ ./configure "$@"
